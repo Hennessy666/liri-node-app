8e0e20b7a0f358bbd15a2b220eab3ed7b56b85bd
github_Hennessy666 <sonjahennessy@gmail.com>
2020-01-18 11:45:00 -0500

    added keys to liri bot and required modules

--- liri.js
+++ liri.js
@@ -1,8 +1,20 @@
+// DEPENDECIES 
+//Read and set environment variables
 require("dotenv").config();
 
+// API Keys
 var keys = require("./keys.js");
+//node-spotify-api NPM package
 var Spotify = require('node-spotify-api');
+//axios npm package
 var axios = require('axios');  //To get the information from the APIs for movie and concert-this
+//import the moment npm package
+var moment = require("moment");
+//import the FS package for read/write
+var fs = require("fs");
+//initialize the spotify API client using our client id and scret
+var spotify = new Spotify(keys.spotify);
+
 
 function spotifySong(song) {
     spotify.search({ type: 'track', query: song, limit: 1 })
@@ -15,7 +27,7 @@ function spotifySong(song) {
 
 function movieOMBD(movie) {
 
-    var movieQuery = movie || "Mr. Nobody"
+    var movieQuery = movie || "Mr. Nobody";
 
     axios.get("http://www.omdbapi.com/?t=" + movieQuery + "&y=&plot=short&tomatoes=true&apikey=trilogy").then(function (response) {
         var movieData =
@@ -26,6 +38,7 @@ function movieOMBD(movie) {
             "\nCountry:" + jsonData.Country +
             "\nLanguage:" + jsonData.Language +
             "\nPlot:" + jsonData.plot +
+            "\nRotten Tomatoes Rating:" + jsonData.Ratings[1] +
             "\nCast:" + jsonData.Actors;
         console.log(movieData);
     })
